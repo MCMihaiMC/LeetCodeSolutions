9b670bbea4abde15aeffec548baa24e71dc5dd34
Muntean C Mihai <muntean.mihai70@gmail.com>
2022-05-30 00:42:50 +0300

    Remove elements started (multiple values)

---
+++ LeetCodeSolutions.Tests/RemoveArrayElementTest.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+
+    public class RemoveArrayElementTest
+    {
+        // Input: nums = [3, 2, 2, 3], val = 3
+        // Output: 2, nums = [2, 2, _, _]
+
+        // Input: nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
+        // Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
+
+        //Input: [0,1,2,2,3,0,4,2], val = 2
+        //Output: [0,1,2,3,0]
+        //Expected: [0,1,4,0,3]
+
+        [Theory]
+        [InlineData(
+        new int[] { 3, 2, 2, 3 }, 2,
+        new int[] { 3, 3 }, 2)]
+        [InlineData(
+        new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2,
+        new int[] { 0, 1, 3, 0, 4}, 5)]
+        public void RemoveElementTest(int[] nums, int val, int[] expectedNums, int expected)
+        {
+            //Arrange & Act
+            var sut = new RemoveArrayElement();
+            int result = sut.RemoveElementV2(nums, val);
+
+            int[] resultArr = new int[result];
+
+            for (int i = 0; i < result; i++)
+                resultArr[i] = nums[i];
+
+            //Assert
+            Assert.Equal(expected, result);
+
+            for (int i = 0; i < result; i++)
+                Assert.Equal(expectedNums[i], resultArr[i]);
+        }
+
+
+//        int[] nums = [...]; // Input array
+//        int val = ...; // Value to remove
+//        int[] expectedNums = [...]; // The expected answer with correct length.
+//                                    // It is sorted with no values equaling val.
+
+//        int k = removeElement(nums, val); // Calls your implementation
+
+//        assert k == expectedNums.length;
+//          sort(nums, 0, k); // Sort the first k elements of nums
+//          for (int i = 0; i<actualLength; i++) {
+//              assert nums[i] == expectedNums[i];
+//         }
+
+    }
+}
---
+++ LeetCodeSolutions/RemoveArrayElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class RemoveArrayElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            nums = nums.Where(v => v != val).ToArray();
+                
+            return nums.Length;
+        }
+
+        public int RemoveElementV2(int[] nums, int val)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == val)
+                {
+                    for (int j = i; j < nums.Length - 1; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+                    Array.Resize(ref nums, nums.Length - 1);
+                    i--;
+                }
+            }
+
+            if(nums[nums.Length - 1] == val)
+                Array.Resize(ref nums, nums.Length - 1);
+
+            //int[] expectedNums = new int[nums.Length];
+
+            //for(int i = 0; i < nums.Length; i++)
+            //{
+            //    expectedNums[i] = nums[i];
+            //}
+
+            return nums.Length;
+        }
+    }
+}
