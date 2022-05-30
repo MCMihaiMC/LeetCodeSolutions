394bed423696a11bbcb8a038ba1dfc4135f618ed
Mihai Muntean <muntean.mihai70@gmail.com>
2022-05-30 09:10:50 +0300

    MountainArray added

---
+++ LeetCodeSolutions.Tests/MountainArrayTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+
+    public class MountainArrayTest
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 2, 1 }, false)]
+        [InlineData(
+            new int[] { 3, 5, 5 }, false)]
+        [InlineData(
+            new int[] { 0, 3, 2, 1 }, true)]
+        [InlineData(
+            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, false)]
+        [InlineData(
+            new int[] { 3, 7, 6, 4, 3, 0, 1, 0 }, false )]
+        public void ValidMountainArrayTest(int[] arr, bool expected)
+        {
+            //Arrange & Act
+            var sut = new MountainArray();
+            bool result = sut.ValidMountainArray(arr);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCodeSolutions/MountainArray.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class MountainArray
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            bool isMountainArray = false;            
+            int maxValue = arr.Max();
+            int maxIndex = arr.ToList().IndexOf(maxValue);
+
+            if (arr.Length <= 2)
+                return false;
+
+            if (maxIndex == arr.Length - 1 || maxIndex == 0)
+                return false;
+
+
+            for (int i = 0; i < maxIndex; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                    isMountainArray = true;
+                else
+                {
+                    return false;
+                }
+            }
+
+            for (int i = maxIndex; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                    isMountainArray = true;
+                else
+                {
+                    return false;
+                }
+            }
+
+            return isMountainArray;
+        }
+    }
+}
