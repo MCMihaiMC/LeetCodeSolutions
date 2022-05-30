7d55bb4243c4539f5be839c502904b25d4fa4bb7
Muntean C Mihai <muntean.mihai70@gmail.com>
2022-05-30 03:31:22 +0300

    Finalized CheckIfNumberDoubleExists

--- LeetCodeSolutions/CheckIfNumberDoubleExists.cs
+++ LeetCodeSolutions/CheckIfNumberDoubleExists.cs
@@ -10,16 +10,50 @@ namespace LeetCodeSolutions
     {
         public bool CheckIfExist(int[] arr)
         {
-            Array.Sort(arr);
-            for (int i = 0; i < arr.Length; i++)
+            // split arrays in 2 (positive vs negative
+            int[] positiveArr = new int[0];
+            int positiveArrIndex = 0;
+            int[] negativeArr = new int[0];
+            int negativeArrIndex = 0;
+
+            for(int i = 0; i < arr.Length; i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
+                if(arr[i] > 0)
                 {
-                    if (arr[i] * 2 == arr[j])
+                    positiveArrIndex++;
+                    Array.Resize(ref positiveArr, positiveArrIndex);
+                    positiveArr[positiveArrIndex - 1] = arr[i];
+                }
+                else
+                {
+                    negativeArrIndex++;
+                    Array.Resize(ref negativeArr, negativeArrIndex);
+                    negativeArr[negativeArrIndex - 1] = arr[i];
+                }
+            }
+
+            Array.Sort(positiveArr);
+            for (int i = 0; i < positiveArr.Length; i++)
+            {
+                for (int j = i + 1; j < positiveArr.Length; j++)
+                {
+                    if (positiveArr[i] * 2 == positiveArr[j])
                         return true;
                 }
             }
+
+            Array.Sort(negativeArr);
+            for (int i = negativeArr.Length; i > 0; i--)
+            {
+                for (int j = negativeArr.Length - 1; j > 0; j--)
+                {
+                    if (negativeArr[i - 1] * 2 == negativeArr[j - 1])
+                        return true;
+                }
+            }
+
             return false;
+
         }
     }
 }
