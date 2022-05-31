2780eb78d3be3320746e5e2a0953a201b5b7dcbf
Mihai Muntean <muntean.mihai70@gmail.com>
2022-05-31 09:39:44 +0300

    InPlace example added

--- LeetCodeSolutions.Tests/InPlaceTests.cs
+++ LeetCodeSolutions.Tests/InPlaceTests.cs
@@ -1,7 +1,25 @@
-﻿namespace LeetCodeSolutions.Tests
+﻿using Xunit;
+
+namespace LeetCodeSolutions.Tests
 {
     public class InPlaceTests
     {
+        [Theory]
+        [InlineData(
+           new int[] { 9, -2, -9, 11, 56, -12, -3 }, 7,
+           new int[] { 81, -2, 81, 11, 3136, -12, 9 } )]
+        public void SquareEvenTest(int[] arr, int len, int[] expected)
+        {
+            //Arrange & Act
+            var sut = new InPlace();
+            int[] result = sut.SquareEven(arr, len);
+
+            //Assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
 
     }
 }
--- LeetCodeSolutions/InPlace.cs
+++ LeetCodeSolutions/InPlace.cs
@@ -2,6 +2,37 @@
 {
     public class InPlace
     {
+        #region InPlace example
+        // InPlace example
+        // Input: array = [9, -2, -9, 11, 56, -12, -3]
+        // Output: [81, -2, 81, 11, 3136, -12, 9]
+        // Explanation: The numbers at even indexes(0, 2, 4, 6) have been squared,
+        // whereas the numbers at odd indexes(1, 3, 5) have been left the same.
+
+        public int[] SquareEven(int[] arr, int len)
+        {
+            if (arr == null)
+            {
+                return null;
+            }
+
+            int[] result = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[i] *= arr[i];
+                }
+                result[i] = arr[i];
+            }
+
+            return result;
+        }
+
+
+        #endregion  
+
 
     }
 }
