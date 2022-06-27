27ea8d905d19cad9059b25784f0431075459fea8
MihaiMuntean <muntean.mihai70@gmail.com>
2022-06-27 18:50:50 +0300

    MaxInterval, interview problem

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCodeSolutions
 {
@@ -7,6 +8,33 @@ namespace LeetCodeSolutions
         static void Main(string[] args)
         {
             Console.WriteLine("LeetCode SOLUTIONS !");
+
+            // CREATE TEST
+            // int excludedNumbersCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> excludedNumbers = new List<int>();
+            excludedNumbers.Add(37);
+            excludedNumbers.Add(7);
+            excludedNumbers.Add(22);
+            excludedNumbers.Add(15);
+            excludedNumbers.Add(49);
+            excludedNumbers.Add(60);
+
+            int excludedNumbersCount = excludedNumbers.Count;
+
+            //for (int i = 0; i < excludedNumbersCount; i++)
+            //{
+            //    int item = Convert.ToInt32(Console.ReadLine().Trim());
+            //    excludedNumbers.Add(item);
+            //}
+
+            int lower = 3; //Convert.ToInt32(Console.ReadLine().Trim());
+
+            int upper = 48; // Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = MaxInterval.GetMaxInterval(excludedNumbers, lower, upper);
+
+            Console.WriteLine(result);
         }
     }
 }
