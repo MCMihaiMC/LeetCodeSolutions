03297b5afe8f7dd97a06320bf6f0e0d583519fde
Mihai Muntean <muntean.mihai70@gmail.com>
2022-07-18 10:09:02 +0300

    Added "HeightChecks"

---
+++ LeetCodeSolutions.Tests/HeightCheckTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+    public class HeightCheckTests
+    {
+        [Theory]
+        [InlineData(
+           new int[] { 1, 1, 4, 2, 1, 3 }, 3,
+           new int[] { 1, 1, 1, 2, 3, 4 })]
+        [InlineData(
+           new int[] { 5, 1, 2, 3, 4 }, 5,
+           new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(
+           new int[] { 1, 2, 3, 4, 5 }, 0,
+           new int[] { 1, 2, 3, 4, 5 })]
+        public void HeightCheckerTest(int[] heights, int wrongPositioned, int[] expected)
+        {
+            //Arrange & Act
+            var sut = new HeightCheck();
+            int result = sut.HeightChecker(heights);
+
+            Array.Sort(heights);
+
+            //Assert
+            Assert.Equal(wrongPositioned, result);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], heights[i]);
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutions/HeightCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class HeightCheck
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int count = 0;
+
+            int[] expectedHeights = new int[heights.Length];
+            for (int i = 0; i < heights.Length; i++)
+            {
+                expectedHeights[i] = heights[i];
+            }
+            Array.Sort(expectedHeights);
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if(heights[i] != expectedHeights[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+}
