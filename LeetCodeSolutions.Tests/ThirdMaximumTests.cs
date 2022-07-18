59ed1904e5a47afc255fe6fdb7befa6ddec358fc
Mihai Muntean <muntean.mihai70@gmail.com>
2022-07-18 13:01:37 +0300

    Added extraction of third maximum value from array / list

---
+++ LeetCodeSolutions.Tests/ThirdMaximumTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace LeetCodeSolutions.Tests
+{
+    public class ThirdMaximumTests
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 3, 2, 1 }, 1)]
+        [InlineData(
+            new int[] { 1, 2 }, 2)]
+        [InlineData(
+            new int[] { 2, 2, 3, 1 }, 1)]
+        [InlineData(
+            new int[] { 1, 2, 2 }, 2)]
+        [InlineData(
+            new int[] { 1, 2, 2, 5, 3, 5 }, 2)]
+        public void ThirdMaxTest(int[] nums, int expected)
+        {
+            //Arrange & Act
+            var sut = new ThirdMaximum();
+            int result = sut.ThirdMax(nums);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCodeSolutions/ThirdMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public class ThirdMaximum
+    {
+        public int ThirdMax(int[] nums)
+        {
+            // Edge case
+            if (nums.Length < 3)
+                return nums.Max();
+
+            // Extract distinct numbers
+            int[] distinctNums = nums.Distinct().ToArray();
+
+            if (distinctNums.Length < 3)
+            {
+                return distinctNums.Max();
+            }
+            else
+            {
+                var distinctNumsList = distinctNums.ToList();
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i < 2)
+                    {
+                        int maxValue = distinctNumsList.Max();
+                        distinctNumsList.RemoveAt(distinctNumsList.IndexOf(maxValue));
+                    }
+                }
+                return distinctNumsList.Max();
+            }
+        }
+    }
+}
